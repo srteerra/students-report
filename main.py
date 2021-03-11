122a4198d66fca28a92f147112ea3661718b4423
Angel Lopez <srterradev@gmail.com>
2021-03-10 17:10:23 -0700

    Fill class and insertstudent

--- main.py
+++ main.py
@@ -1,29 +1,40 @@
+# Turn A: Thursday - Tuesday (5pm-7pm)
+# Turn B: Saturday (8am-1pm)
+
 # [piano[0], guitar[1], violin[2]]
-# guitar and violin have maximum places to 12 people
 subject = [[],[],[]]
 
-def InsertStudent(nameVar, ageVar, groupVar, turnVar):
-    students = [nameVar, ageVar, groupVar, turnVar]
+class Fill:
 
-    if(groupVar.lower() == "piano"):
-        if(len(subject[0]) < 12):
-            subject[0].append(students)
-        else:
-            print("The piano class is already full")
-    elif(groupVar.lower() == "guitar"):
-        if(len(subject[1]) < 12):
-            subject[1].append(students)
-        else:
-            print("The guitar class is already full")
-    elif(groupVar.lower() == "violin"):
-        subject[2].append(students)
+    def __init__(self, nameVar, ageVar, groupVar, turnVar):
+        students = [nameVar, ageVar, groupVar, turnVar]
 
-InsertStudent(
-    input("Enter a name: ").lower(),
-    input("Enter an age: "),
-    input("Enter a group: ").lower(),
-    input("Enter a turn: ").lower()
-)
+        # guitar and violin have maximum places to 12 people
+        if(groupVar.lower() == "piano"):
+            if(len(subject[0]) < 12):
+                subject[0].append(students)
+            else:
+                print("The piano class is already full")
+        elif(groupVar.lower() == "guitar"):
+            if(len(subject[1]) < 12):
+                subject[1].append(students)
+            else:
+                print("The guitar class is already full")
+        elif(groupVar.lower() == "violin"):
+            subject[2].append(students)
+
+class insertstudent:
+
+    def __init__(self):
+        self.name = input("Enter a name: ").lower()
+        self.age = input("Enter an age: ")
+        self.group = input("Enter a group: ").lower()
+        self.turn = input("Enter a turn: ").lower()
+
+        Fill(self.name, self.age, self.group, self.turn)
+
+class __init__:
+    insertstudent()
 
 print(subject[0])
 print(subject[1])
